2eaf689805e69206ca103f647874833486297bb9
Luant <luanluong960@gmail.com>
2025-10-18 13:43:35 +0700

    finish detail Product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
