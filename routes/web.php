ffbe0f49531531457066824c221464cbbc69c042
Luant <luanluong960@gmail.com>
2025-10-18 13:44:40 +0700

    finish detailProduct

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('home');
 });
+
 Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
