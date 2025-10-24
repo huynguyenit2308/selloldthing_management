e77a271364634670c26a3b78c45fb66526d9bc33
ngodanhloi <ng.du2020@gmail.com>
2025-10-24 22:47:41 +0700

    commit lai database goc

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -22,8 +22,8 @@ return new class extends Migration
         });
 
         Schema::create('job_batches', function (Blueprint $table) {
-            $table->string('id',100)->primary();
-            $table->string('name',100);
+            $table->string('id')->primary();
+            $table->string('name');
             $table->integer('total_jobs');
             $table->integer('pending_jobs');
             $table->integer('failed_jobs');
--- database/migrations/2025_10_14_055919_create_products_table.php
+++ database/migrations/2025_10_14_055919_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name',100);
+            $table->string('name');
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->decimal('price', 10, 2);
             $table->text('description')->nullable();
